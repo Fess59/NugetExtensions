a8c3b49d83372d8b62d7300db8868003f8c2b2b6
Fess <Kozhevnikov.A.M@yandex.ru>
2018-01-19 20:01:41 +0300

    #13 https://www.nuget.org/packages/FessooFramework/

---
+++ FessooFramework/FessooFramework/BLL/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using FessooFramework.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FessooFramework.BLL
+{
+    /// <summary>   Модуль управления загрузкой системы. Для загрузки рекомендуется использовать реализацию Singlton </summary
+    /// <remarks>   AM Kozhevnikov, 19.01.2018. </remarks>
+
+    public class Bootstrapper
+    {
+        #region Singlton Lazy Threadsafe
+        public static Bootstrapper Current {  get { return getInstance(); } }
+        private static Bootstrapper getInstance()
+        {
+            return Nested.current;
+        }
+        private class Nested
+        {
+            internal static readonly Bootstrapper current = new Bootstrapper();
+        }
+        #endregion
+        #region Constructor
+        public Bootstrapper()
+        {
+            ConsoleHelper.SendWarning("Рекомендуется использовать Singlton в нём реализован процесс настройки и повторного использования. Bootstrapper.Current");
+        }
+        public Bootstrapper(string s)
+        {
+           
+        }
+        #endregion
+    }
+}
---
+++ FessooFramework/FessooFramework/Tools/ConsoleHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FessooFramework.Tools
+{
+    /// <summary>   A console helper.
+    ///             Для взаимодействия с консолью
+    ///             Помогает централизовано управлять выводом в консоль, его оформлением и самой публикацией в при переключении debug и релиз
+    ///             </summary>
+    ///
+    /// <remarks>   AM Kozhevnikov, 19.01.2018. </remarks>
+
+    public static class ConsoleHelper
+    {
+        /// <summary>   Send this message.
+        ///             Отправка данных в консоль с настроенным форматом
+        ///             Работает при наличии символов условной компиляции DEBUG</summary>
+        ///
+        /// <remarks>   AM Kozhevnikov, 19.01.2018. </remarks>
+        ///
+        /// <param name="text"> The text. </param>
+
+        public static void Send(string text)
+        {
+#if DEBUG
+            Console.WriteLine($"[{DateTime.Now.ToString("o")}] {text}");
+#endif
+        }
+
+        /// <summary>   Sends a warning. Отправляет предупреждение в консоль </summary>
+        ///
+        /// <remarks>   AM Kozhevnikov, 19.01.2018. </remarks>
+        ///
+        /// <param name="text"> The text. </param>
+
+        public static void SendWarning(string text)
+        {
+            Send($"WARNING! {text}");
+        }
+    }
+}
